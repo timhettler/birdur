0caff6e1f58317ebf2f7c15556f50af67ad84102
Tim Hettler <me@timhettler.com>
2013-08-01 15:35:10 -0400

    search field works

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,65 @@
 
 /* Controllers */
 
-birdur.controller('SplashCtrl', function ($location, GeoLoc) {
+birdur.controller('SplashCtrl', function ($scope, $location, GeoLoc, LocationService) {
   GeoLoc.locate()
     .then(function (position) {
       $location.path('/map/'+position.coords.latitude+','+position.coords.longitude);
     });
+
+  $scope.handleUserInput = function(query) {
+    var regex_latLng = /^(-?\d{1,3}(\.\d*)?),\s?(-?\d{1,3}(\.\d*)?)$/,
+        regex_whitespace = /\s+/g,
+        regex_zipcode = /^\d{5}(-\d{4})?$/g,
+        regex_address = /^(.*)?(\w+)?(,?\s?)?(\w)+(,?\s?)(\w)+/,
+        regex_addressSeparator = /,\s?/,
+        geosearchQuery = null;
+
+    if(!query) {
+      query = $scope.searchQuery;
+    }
+
+    query = query
+              .trim()
+              .replace(regex_whitespace, ' ')
+              .toLowerCase();
+
+    if(regex_latLng.test(query)) {
+      $scope.handleLocationData.apply(this, query.split(','));
+    } else {
+
+      if(regex_zipcode.test(query)) {
+        geosearchQuery = "[zip="+query+"][country=United States]";
+      } else if(regex_address.test(query)) {
+        var address = query.split(regex_addressSeparator);
+        if(address.length == 1) {
+          geosearchQuery = "[city="+address[0]+"][country=United States]";
+        } else if(address.length == 2) {
+          geosearchQuery = "[city="+address[0]+"][state="+address[1]+"][country=United States]";
+        }
+        else if(address.length == 3) {
+          geosearchQuery = "[street="+address[0]+"][city="+address[1]+"][state="+address[2]+"][country=United States]";
+        }
+        else if(address.length == 4) {
+          geosearchQuery = "[housenumber="+address[0]+"][street="+address[1]+"][city="+address[2]+"][state="+address[3]+"][country=United States]";
+        }
+      }
+
+      if(geosearchQuery) {
+        console.log(geosearchQuery);
+        LocationService.geosearch({
+          q: geosearchQuery
+        }, function (data) {
+          if(data.places) {
+            var coords = data.places[0].position;
+            $location.path('/map/'+coords.lat+','+coords.lon);
+          } else {
+            //TODO
+          }
+        });
+      }
+    }
+  };
 });
 
 birdur.controller('HotspotsListCtrl', function ($scope, $routeParams, $location, Map, GeoLoc, eBird, LocationService) {
@@ -137,7 +191,7 @@ birdur.controller('HotspotsListCtrl', function ($scope, $routeParams, $location,
         });
       }
     }
-  }
+  };
 
   $scope.GeoLocate = function () {
     GeoLoc.locate()
